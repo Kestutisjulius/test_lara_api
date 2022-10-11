39c7086d8bfee49f180df3b0f1063c719cccf7ef
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-10-11 05:40:47 +0300

    Examle show

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController as Category;
+use App\Http\Controllers\Api\ProductController as Product;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('categories', [Category::class, 'index']);
+Route::get('categories/{category}', [Category::class, 'show']);
+
+Route::get('products', [Product::class, 'index']);
+Route::get('product/{product}', [Product::class, 'show']);
