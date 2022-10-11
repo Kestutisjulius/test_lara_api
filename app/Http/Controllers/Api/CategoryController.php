a4c8c9e109e2502f28d7cf5f544c9171142c7187
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-10-11 08:43:47 +0300

    Requests

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -10,14 +11,22 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index(){
+
         $categories = Category::all();
+
         return CategoryResource::collection($categories);
     }
+
     public function show(Category $category){
+
         return new CategoryResource($category);
+
     }
-    public function store(Request $request){
+
+    public function store(StoreCategoryRequest $request){
+
         $category = Category::create($request->all());
+
         return new CategoryResource($category);
     }
 }
